f0e4b0cb292f88d524b849c7931146cd3177698b
mildhasvoided <mildhasvoided@gmail.com>
2026-04-08 17:07:01 +0000

    upstream

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-Main branch of Booksynn.
-
-For new features (untested) go to the <a href="https://github.com/mildhasvoided/Booksync-Beta-branch-.git">BETA BRANCH</a>
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca3fc915539ff737e2be5b7c695f93d04ae920c481cdbaa335f304c2df6a7c7c
+size 150
--- package.sh
+++ package.sh
@@ -1,36 +1,3 @@
-#!/bin/bash
-
-echo "1: package"
-echo "2: unpackage"
-read -rp "> " choice
-
-case "$choice" in
-  1)
-    if [ ! -d "Booksync" ]; then
-      echo "Error: 'Booksync' directory not found."
-      exit 1
-    fi
-    zip -r Booksync.zip Booksync
-    echo "Done: Booksync.zip created."
-    ;;
-  2)
-    if [ ! -f "Booksync.zip" ]; then
-    clear
-      echo "Error: 'Booksync.zip' not found."
-      exit 1
-    fi
-    if [ -d "Booksync" ]; then
-      mv "Booksync" "Booksync (Old)"
-      echo "Renamed existing 'Booksync' to 'Booksync (Old)'."
-    fi
-    unzip Booksync.zip
-    clear
-    echo "Done: Booksync extracted."
-    ;;
-  *)
-    clear
-    echo "Invalid choice. Enter 1 or 2."
-
-    exit 1
-    ;;
-esac
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1d4a269b809265d55ae28047326796a6883c5d3f686822cba53ca48933e6d4b
+size 692
